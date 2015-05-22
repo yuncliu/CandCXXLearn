f7187f36bc4212e2ec941fe1067c87d2ce6618bf
yuncliu <liuyuncheng90@gmail.com>
2015-05-22 15:04:44 +0800

    add block read

--- kernel/cdev/char.c
+++ kernel/cdev/char.c
@@ -4,16 +4,19 @@
 #include <linux/cdev.h>
 #include <linux/ioctl.h>
 #include <linux/uaccess.h>
+#include <linux/sched.h>
+#include <linux/wait.h>
 #include "char.h"
 
 
 //static int Major = 0;
 static int Major = 0;
-static int IsOpen = 0;
+static int OpenCount = 0;
 
 int my_init_module(void);
 void my_cleanup_module(void);
 static char msg[10] = {0};
+static char kbuf[10] = {0};
 
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
@@ -29,11 +32,14 @@ static struct file_operations fops = {
     .release = device_release
 };
 
+DECLARE_WAIT_QUEUE_HEAD(wq);
+
 
 int my_init_module() {
     printk("init module\n");
     Major = register_chrdev(0, DEVICE_NAME, &fops);
     printk("Mator = [%d]\n", Major);
+    printk("'mknod /dev/mdevv c %d 0'.\n", Major);
     return 0;
 }
 
@@ -42,28 +48,42 @@ void my_cleanup_module() {
 }
 
 static int device_open(struct inode* node, struct file* file) {
-    if (IsOpen) {
-        printk("already opened [%d]\n", IsOpen);
-        return -1;
-    }
-    printk("open\n");
-    IsOpen++;
+    OpenCount++;
+    printk("open. currently opened [%d]\n", OpenCount);
 
     return 0;
 }
 
 static int device_release(struct inode* node, struct file* file) {
-    printk("close\n");
-    IsOpen--;
+    OpenCount--;
+    printk("close. currently opened [%d]\n", OpenCount);
     return 0;
 }
 
 static ssize_t device_read(struct file* file, char* buf, size_t size, loff_t* offset) {
-    memset(buf, 0xAA, size);
+    if (size != 10) {
+        printk("read error\n");
+        return -1;
+    }
+    printk("read\n");
+    while(strlen(kbuf) == 0 )
+    {
+        interruptible_sleep_on(&wq);
+    }
+    printk("read kbuf = [%s]\n", kbuf);
+    copy_to_user(buf, kbuf, 10); 
+    memset(kbuf, 0x00, sizeof(kbuf));
     return size;
 }
 
 static ssize_t device_write(struct file* file, const char* buf, size_t size, loff_t* offset) {
+    if (size != 10) {
+        printk("write error\n");
+        return -1;
+    }
+    printk("write\n");
+    copy_from_user(kbuf, buf, 10); 
+    wake_up_interruptible(&wq);
     return size;
 }
 
--- kernel/cdev/main.c
+++ kernel/cdev/main.c
@@ -6,9 +6,17 @@
 
 #include "char.h"
 
+int call_ioctl();
+int call_read();
+int call_write();
 
 int main(int argc, char* argv)
 {
+    call_read();
+    return 0;
+}
+
+int call_ioctl() {
     int fd = open("/dev/mdev", O_CREAT, S_IRWXU);
     char hellow[10] = {0};
     snprintf( hellow, 10, "%s", "hellow");
@@ -18,6 +26,30 @@ int main(int argc, char* argv)
     ioctl(fd, IOCTL_XGET_MSG, buf);
     printf("get [%s]\n", buf);
 
+    close(fd);
+    return 0;
+}
+
+int call_read() {
+    int fd = open("/dev/mdev", O_CREAT, S_IRWXU);
+    char buf[10] = {0};
+    read(fd, buf, 10);
+    printf("read [%s]\n", buf);
+    return 0;
+}
+
+int call_write() {
+    int fd = open("/dev/mdev", O_CREAT | O_APPEND | O_RDWR);
+    if (fd < 0) {
+        printf("%s\n", strerror(errno));
+        return -1;
+    }
+    char buf[10] = {0};
+    sprintf(buf, "%s", "hellow");
+    ssize_t w = write(fd, buf, 10);
+    if (w < 0) {
+        printf("%s\n", strerror(errno));
+    }
     close(fd);
     return 0;
 }
